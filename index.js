7a056f8e3212f0d4c502739d5cdf9e118f774184
Richard Wohlbold <rigewo02@gmail.com>
2019-09-03 16:16:57 +0200

    Use one data structure; broadcast members on connect

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const server = require('http').createServer();
 const io = require('socket.io')(server);
 
-const clients  = [];
-const usernames = {};
+const clients = {};
 
 io.on('connection', client => {
-    clients.push(client);
+    clients[client.id] = {client, username: client.id + ""};
+    const members = Object.values(clients).map(c => c.username);
+    client.emit("members", members);
     client.on('message', data => {
-        clients.forEach(c => c.emit("message", usernames[client.id] + ": " + data))
+        for (var c in clients) {
+            clients[c].client.emit("message", clients[c].username + ": " + data);
+        }
     });
-    client.on('username', data => usernames[client.id] = data);
-    client.on('disconnect', () => {
-        clients
-
+    client.on('username', data => {
+        clients[client.id].username = data;
+        for (var c in clients) {
+            const members = Object.values(clients).map(c => c.username)
+            clients[c].client.emit("members", members)
+        }
+    });
+    client.on('disconnect', c => {
+        delete clients[client.id]
     });
 });
 server.listen(3000);
